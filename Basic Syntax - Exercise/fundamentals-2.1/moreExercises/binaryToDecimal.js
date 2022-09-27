8c15c9e4ac7c4d29fdfdc78180d5dffc631b75c6
MartotoBG <martingav123@gmail.com>
2022-09-27 11:58:26 +0300

    added solutions

--- Basic Syntax - Exercise/fundamentals-2.1/moreExercises/binaryToDecimal.js
+++ Basic Syntax - Exercise/fundamentals-2.1/moreExercises/binaryToDecimal.js
@@ -1,28 +1,17 @@
 function binaryToDecimal (binary) {
     let binaryToString = binary.toString();
     let decimal = 0;
-    if (binaryToString[7] == 1){
-        decimal+=1;
+    let length = binaryToString.length;
+    let one = 1;
+    for (let i = length-1; i>=0 ; i--){
+        if (binaryToString[i] == 0){
+        }
+        else {
+            decimal+=one;
+        }
+        one *= 2;
     }
-    else {
-        decimal+=0
-    }
-   
-    decimal = binaryToString[6] ==1 ? decimal +=2 : decimal+=0;
-
-    decimal = binaryToString[5] ==1 ? decimal +=4 : decimal+=0;
-
-    decimal = binaryToString[4] ==1 ? decimal +=8 : decimal+=0;
-
-    decimal = binaryToString[3] ==1 ? decimal +=16 : decimal+=0;
-
-    decimal = binaryToString[2] ==1 ? decimal +=32 : decimal+=0;
-
-    decimal = binaryToString[1] ==1 ? decimal +=64 : decimal+=0;
-
-     decimal = binaryToString[0] ==1 ? decimal +=128 : decimal+=0;
-
-    console.log(decimal)
+    console.log(decimal);
 }
 
-binaryToDecimal ("00010101")
+binaryToDecimal ("00001001")
