8d1e057dbd62b7095b2a2276adbc52f6ccecf657
MartotoBG <martingav123@gmail.com>
2022-10-02 02:38:11 +0300

    added solutions

--- Fundamentals/fundamentals-3.1/magicSum.js
+++ Fundamentals/fundamentals-3.1/magicSum.js
@@ -1,32 +1,13 @@
-function findMagicSum (numbers, sum){
+function magicSum (numbers, sum){
     let length = numbers.length;
-    let magic = [] + ``;
-    for (let i = 0 ; i<length ; i++){
-        for (let j = 0; j<length ; j++){
-            
-            if (numbers[i] + numbers[j] == sum){
-                if (numbers[i] == numbers[j]){
-                    break;
-                }
-                else {
-                magic = numbers[i] + ` ` + numbers[j];
-                console.log(magic);
-                for (let z = 0;z<length;z++){
-                    for (let y = 0; y <length; y++){
-                        if (numbers[i] == numbers[z]){
-                            numbers[z] += " a ";
-                        }
-                        else if (numbers[j] == numbers[y]){
-                            numbers[y] += " a ";
-                        }
-                    }
-                }
-                }
+   
+    for (let i = 0 ; i < length ; i++){
+        for (let j = i + 1; j < length ; j++){
+            if (numbers[i] + numbers[j] === sum){
+                console.log(`${numbers[i]} ${numbers[j]}`);
             }
-        }  
+        }
     }
 }
 
-
-
-findMagicSum ([-1,0,1,-2], -1);
+magicSum ([2,2,2,4,4,4], 6)
