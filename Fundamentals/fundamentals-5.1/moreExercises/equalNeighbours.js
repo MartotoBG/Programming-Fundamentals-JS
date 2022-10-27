a0886786634d15607ffcf0c1087b885d083d144b
MartotoBG <martingav123@gmail.com>
2022-10-27 14:05:14 +0300

    added solutions

--- Fundamentals/fundamentals-5.1/moreExercises/equalNeighbours.js
+++ Fundamentals/fundamentals-5.1/moreExercises/equalNeighbours.js
@@ -7,8 +7,8 @@ function equal(bigArray) {
         let previousArray = bigArray[i - 1];
         let nextArray = bigArray[i + 1];
         for (let j = 0; j < rowLength; j++) {
-            if (i == 0) {
-                if (j == 0) {
+            if (i == 0) { // за първия масив
+                if (j == 0) { // тоест е първото число от първия масив и затова проверявам само отдясно и отдолу
                     if (rowArray[j] == rowArray[j + 1] ) {
                         count++;
                     }
@@ -16,7 +16,7 @@ function equal(bigArray) {
                         count++;
                     }
                 }
-                else if (j == rowLength - 1) {
+                else if (j == rowLength - 1) { // тоест е последното число от първия масив и затова проверявам само отдолу и отляво
                     if (rowArray[j] == rowArray[j - 1]) {
                         count++;
                     }
@@ -24,8 +24,8 @@ function equal(bigArray) {
                         count++;
                     }
                 }
-                else {
-                    if (rowArray[j] == rowArray[j - 1]) {
+                else { // за всички останали в първия масив, които не са първо и последно число следователно проверяваме дали са еднакви с числото отдолу, отляво и отдясно
+                    if (rowArray[j] == rowArray[j - 1]) { 
                         count++;
                     }
                     if(rowArray[j] == nextArray[j]){
@@ -36,9 +36,9 @@ function equal(bigArray) {
                     }
                 }
             }
-            else if (i == length - 1) {
+            else if (i == length - 1) { // за последния масив
 
-                if (j == 0) {
+                if (j == 0) { // за първия елемент от последния масив затова проверявам дали са еднакви само с отдясно и отгоре
                     if (rowArray[j] == rowArray[j + 1]) {
                         count++;
                     }
@@ -46,7 +46,7 @@ function equal(bigArray) {
                         count++;
                     }
                 }
-                else if (j == rowLength - 1) {
+                else if (j == rowLength - 1) { // за последния елемент от последния масив затова проверявам само с отляво и отгоре
                     if (rowArray[j] == rowArray[j - 1]) {
                         count++;
                     }
@@ -54,7 +54,7 @@ function equal(bigArray) {
                         count++;
                     }
                 }
-                else {
+                else { // за всички останали в първия масив, които не са първо и последно число следователно проверяваме дали са еднакви с числото отгоре, отляво и отдясно
                     if (rowArray[j] == rowArray[j - 1]) {
                         count++;
                     }
@@ -66,7 +66,7 @@ function equal(bigArray) {
                     }
                 }
             }
-            else {
+            else { // ако сме стигнали до този else това означава че числото има съседно от четирите страни и проверяваме всяко дали се повтаря
                 if (rowArray[j] == rowArray[j + 1]) {
                     count++;
                 }
@@ -82,7 +82,11 @@ function equal(bigArray) {
             }
         }
     }
-    console.log(count / 2);
+    console.log(count / 2); // делим на две тъй като като минаваме през числата по лигиката горе, която съм написал, 
+    //то всяка двойка ще ми я брои два пъти, примерно ако имаме първи масив започващ с "2" "2", 
+    //то първо ще ги преброи като индекс 0 = индекс 1 и ще приключи операцията, после отива на индекс 1 и ще го провери
+    // и ще види че е равен на индекс 0 и така ще го преброи 2 пъти
+    // надявам се да сте ме разбрали защо го делим на 2 :D
 }
 
 equal(
