9345e8a20242e96e5ae8337e4466913ac7470f75
MartotoBG <martingav123@gmail.com>
2022-11-08 12:21:52 +0200

    added solutions

--- Fundamentals/fundamentals-6.2/cardGame.js
+++ Fundamentals/fundamentals-6.2/cardGame.js
@@ -1,12 +1,52 @@
-function cardHand (array){
-    for (let i = 0 ; i<array.length; i++){
-    let person = array.shift();
-    person = person.split(`, `);
-    let name = person.shift();
-    let cards = person
-    console.log(person)
+function cardHand(arr) {
+    const players = new Map(); // playerName => set of player cards
+    const power = {
+        '2': 2,
+        '3': 3,
+        '4': 4,
+        '5': 5,
+        '6': 6,
+        '7': 7,
+        '8': 8,
+        '9': 9,
+        '10': 10,
+        'J': 11,
+        'Q': 12,
+        'K': 13,
+        'A': 14
+    };
+    const multiplier = {
+        'S': 4,
+        'H': 3,
+        'D': 2,
+        'C': 1
+    };
+ 
+    for (const entry of arr) {
+        let [name, cards] = entry.split(': ');
+ 
+        if (!players.has(name)) {
+            players.set(name, new Set(cards.split(', ')));
+        }
+        else {
+            for (const card of cards.split(', ')) {
+                players.get(name).add(card);
+            }
+        }
     }
-    //(S -> 4, H-> 3, D -> 2, C -> 1).
+ 
+    const playersPower = new Map(); // playerName => totalPower
+ 
+    [...players.entries()].forEach(([name, cards]) => {
+        const score = [...cards].map(x => {
+            const {p, m} = x.match(/(?<p>\d+|J|Q|K|A)(?<m>[SHDC])/).groups;
+            return power[p] * multiplier[m];
+        }).reduce((x, y) => x + y, 0);
+ 
+        playersPower.set(name, score);
+    });
+ 
+    [...playersPower.entries()].forEach(([name, pow]) => console.log(`${name}: ${pow}`));
 }
 
 cardHand ([
