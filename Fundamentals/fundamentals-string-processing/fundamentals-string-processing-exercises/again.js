5fd6a18f9e9b194f24ed62190a03f28ea8eff651
MartotoBG <martingav123@gmail.com>
2022-11-17 00:02:39 +0200

    added solutions

--- Fundamentals/fundamentals-string-processing/fundamentals-string-processing-exercises/again.js
+++ Fundamentals/fundamentals-string-processing/fundamentals-string-processing-exercises/again.js
@@ -1,23 +1,47 @@
 function again (array){
     const object = {};
-    array.forEach(person => {
-        const length = person.length;
-        object[person] = length;
+    let movies = [];
+    array.forEach(element => {
+        if(element.includes('addMovie')){
+            element = element.split(' ');
+            const action = element.shift();
+            element = element.join(' ');
+            const name = element
+            movies.push(element);
+            object[name] = [];
+        }
+        else if (element.includes('directedBy')){
+          const  [name,director] = element.split(' directedBy ');
+            if(!object[name]){
+                object[name] = director
+            }
+        }
+        
     });
-    for(const person in object){
-        console.log(`Name: ${person} -- Personal Number: ${object[person]}`)
-    }
-    //Name: {employeeName} -- Personal Number: {personalNum}
+    // "addMovie {movie name}" – add the movie
+
+    // · "{movie name} directedBy {director}" – check if the movie exists and then add the director
+    
+    // · "{movie name} onDate {date}" – check if the movie exists and then add the date
+
 }
 
 again([
 
-    'Silas Butler',
+    'addMovie Fast and Furious',
     
-    'Adnaan Buckley',
+    'addMovie Godfather',
     
-    'Juan Peterson',
+    'Inception directedBy Christopher Nolan',
     
-    'Brendan Villarreal'
+    'Godfather directedBy Francis Ford Coppola',
+    
+    'Godfather onDate 29.07.2018',
+    
+    'Fast and Furious onDate 30.07.2018',
+    
+    'Batman onDate 01.08.2018',
+    
+    'Fast and Furious directedBy Rob Cohen'
     
     ])
