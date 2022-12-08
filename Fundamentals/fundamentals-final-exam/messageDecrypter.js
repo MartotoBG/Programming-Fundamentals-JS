4b7195d1c101bd83890e24a779f27c0ce9014255
MartotoBG <martingav123@gmail.com>
2022-12-08 15:16:55 +0200

    added solutions

--- Fundamentals/fundamentals-final-exam/messageDecrypter.js
+++ Fundamentals/fundamentals-final-exam/messageDecrypter.js
@@ -1,50 +1,34 @@
-function message(array) {
-    let pattern = /^([$%])(?<tag>[A-Z]{1}[a-z]{2,})\1: \[(?<number1>\d+)\]\|\[(?<number2>\d+)\]\|\[(?<number3>\d+)\]\||$/g
-    let tags = Number(array.shift());
-    for (let i = 0; i < tags; i++) {
-        let match = array[i].match(pattern);
-        if (match[0].length > 0) {
-
-            let name = match.join('').split(': ')[0];
-            let newName = '';
-            let numbers = match.join('').split(': ')[1].split('|');
-            let newNumbers = '';
-            let decrypt = '';
-            for (let i = 0; i < numbers.length-1; i++) {
-                for (let j = 1; j < numbers[i].length-1; j++) {
-                   
-                    newNumbers += numbers[i][j]; 
-                }
-                newNumbers += ' ';
-            }
-            newNumbers = newNumbers.split(' ');
-            newNumbers.pop()
-            console.log(newNumbers.length)
-           if (newNumbers.length<=3){
-            for (let i = 0; i<newNumbers.length; i++){
-                newNumbers[i] = Number(newNumbers[i]);
-                getCurrentChar = String.fromCharCode(newNumbers[i]);
-                decrypt+=getCurrentChar;
-                
-            }
-            console.log(decrypt)
+function messageDecrypter(input){
+    let countTags = Number(input.shift());
+        pattern = /^([$%])(?<tag>[A-Z]{1}[a-z]{2,})\1: \[(?<number1>\d+)\]\|\[(?<number2>\d+)\]\|\[(?<number3>\d+)\]\|$/g
+        //           /^([$%])(?<tag>[A-Z][a-z]{2,})\1: \[(?<number1>\d+)\]\|\[(?<number2>\d+)\]\|\[(?<number3>\d+)\]\|$/g;
+    for (let index = 0; index < countTags; index++) {
+        let tagRow = input[index];
+        let valid = pattern.exec(tagRow);
+ 
+        if (valid) {
             
-           
-            for (let i = 1; i < name.length - 1; i++) {
-                newName += name[i];
-            }
-           
-       }
-    }
-        else {
+                const tag = valid.groups['tag']
+                const firstNumber = valid.groups['number1'];
+                const secondNumber = valid.groups['number2'];
+                const lastNumber = valid.groups['number3'];
+ 
+                let result = String.fromCharCode(firstNumber, secondNumber, lastNumber);
+                console.log(`${tag}: ${result}`);
+ 
+                
+            
+        } else {
             console.log('Valid message not found!');
         }
-
+        valid = pattern.exec(tagRow);
+        tagRow = input[index];
+        
     }
 }
 
-message((["4",
-    "$Request$: [73]|[115]|[105]|",
-    "%Taggy$: [73]|[73]|[73]|",
-    "%Taggy%: [118]|[97]|[108]|",
-    "$Request$: [73]|[115]|[105]|[32]|[75]|"]))
+messageDecrypter (((["4",
+"$Request$: [73]|[115]|[105]|",
+"%Taggy$: [73]|[73]|[73]|",
+"%Taggy%: [118]|[97]|[108]|",
+"$Request$: [73]|[115]|[105]|[32]|[75]|"])))
